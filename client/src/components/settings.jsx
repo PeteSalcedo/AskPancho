bf9d79a8f009a9e8373270b91e575df05f7aff55
PeteSalcedo <PEter.salcedo.diaz@gmail.com>
2018-10-04 15:43:12 -0500

    uploading the changes of change users pictures

--- client/src/components/settings.jsx
+++ client/src/components/settings.jsx
@@ -1,14 +1,24 @@
 import React, { Component } from "react";
 import Status from './status.jsx';
 import ChangePicture from './changePicture.jsx';
+import firebase from "firebase";
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"; 
 
 class Settings extends Component {
 
   render() {
     return (
       <div>
-        <Status />
-        <ChangePicture />
+        var user = firebase.auth().currentUser;
+
+      user.updateProfile({
+        displayName: "Jane Q. User",
+        photoURL: "https://example.com/jane-q-user/profile.jpg"
+      }).then(function() {
+        // Update successful.
+      }).catch(function(error) {
+        // An error happened.
+      });
       </div>
     )
   }
