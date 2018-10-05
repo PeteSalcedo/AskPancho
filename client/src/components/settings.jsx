1678e9f9fc6e3333414bc7c7e962e2ff826788a0
PeteSalcedo <PEter.salcedo.diaz@gmail.com>
2018-10-04 23:23:58 -0500

    Uploading changes to the git ignore file api keys

--- client/src/components/settings.jsx
+++ client/src/components/settings.jsx
@@ -2,23 +2,31 @@ import React, { Component } from "react";
 import Status from './status.jsx';
 import ChangePicture from './changePicture.jsx';
 import firebase from "firebase";
-import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"; 
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth";
 
 class Settings extends Component {
+  getNewPhoto(){
+    //pick user phot from device
+    //pick user photo from some photoURL
 
+    //return value should be some url
+  }
+updateProfile(){
+  var user = firebase.auth().currentUser;
+
+user.updateProfile({
+  displayName: user,
+  // firebase.auth().currentUser.photoURL  getNewPhoto()
+}).then(function() {
+  // Update successful.
+}).catch(function(error) {
+
+});
+}
   render() {
     return (
       <div>
-        var user = firebase.auth().currentUser;
 
-      user.updateProfile({
-        displayName: "Jane Q. User",
-        photoURL: "https://example.com/jane-q-user/profile.jpg"
-      }).then(function() {
-        // Update successful.
-      }).catch(function(error) {
-        // An error happened.
-      });
       </div>
     )
   }
